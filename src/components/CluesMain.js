fdf9f9f806f1d2951372e8cb6c75b05b9f0001ec
Jaymes Young <jaymes@phoenixjaymes.com>
2019-12-22 14:56:05 -0800

    initial commit

---
+++ src/components/CluesMain.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CluesMain = ({}) => (
+  <div>
+    CluesMain
+  </div>
+);
+
+CluesMain.propTypes = {
+
+};
+
+export default CluesMain;
