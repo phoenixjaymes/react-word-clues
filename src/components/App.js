4d1c9531c144ee1d27c30150366a4c0b12bfa2d7
Jaymes Young <jaymes@phoenixjaymes.com>
2019-12-22 16:06:11 -0800

    added useState 4 section, created handleSection func

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/App.css';
 
+import Home from './Home';
+import CluesMain from './CluesMain';
+
 function App() {
+  const [section, setSection] = useState('home');
+
+  const handleSectionClick = (sectionName) => {
+    setSection(sectionName);
+  };
+
+  if (section === 'clues') {
+    return (
+      <CluesMain
+        handleSectionClick={handleSectionClick}
+      />
+    );
+  }
+
   return (
-    <div className="App">
-      ass
-    </div>
+    <Home
+      handleSectionClick={handleSectionClick}
+    />
   );
 }
 
