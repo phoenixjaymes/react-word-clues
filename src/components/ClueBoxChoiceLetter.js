f997556328d238cd06bd4f9bb736cba85dda2a98
Jaymes Young <jaymes@phoenixjaymes.com>
2019-12-24 10:44:01 -0800

    copy of answerLetter

---
+++ src/components/ClueBoxChoiceLetter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from '../css/ClueBoxChoiceLetter.module.css';
+
+const ClueLetter = ({ children }) => {
+
+  return (
+    <div className={styles.wrap}>
+      <span>{children}</span>
+    </div>
+  );
+};
+
+ClueLetter.propTypes = {
+  children: PropTypes.string,
+};
+
+export default ClueLetter;
