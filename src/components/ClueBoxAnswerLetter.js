36481d608d94ae737cfabfd3f39950d01fd9d7e0
Jaymes Young <jaymes@phoenixjaymes.com>
2019-12-24 12:43:08 -0800

    added if to return button

--- src/components/ClueBoxAnswerLetter.js
+++ src/components/ClueBoxAnswerLetter.js
@@ -4,7 +4,13 @@ import PropTypes from 'prop-types';
 import styles from '../css/clueBoxAnswerLetter.module.css';
 
 const ClueBoxAnswerLetter = ({ children }) => {
-
+  if (children !== '') {
+    return (
+      <div className={styles.wrap}>
+        <button className={styles.button} type="button">{children}</button>
+      </div>
+    );
+  }
   return (
     <div className={styles.wrap}>
       <span>{children}</span>
