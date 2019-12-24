051f8b30379b8d92716ad710cf51c84bbaf26559
Jaymes Young <jaymes@phoenixjaymes.com>
2019-12-24 14:11:00 -0800

    chnaged name of value prop

--- src/components/ClueBoxAnswerLetter.js
+++ src/components/ClueBoxAnswerLetter.js
@@ -4,16 +4,16 @@ import PropTypes from 'prop-types';
 import styles from '../css/clueBoxAnswerLetter.module.css';
 
 const ClueBoxAnswerLetter = ({ letter, handleAnswerClick }) => {
-  if (letter.value !== '') {
+  if (letter.choiceLetter !== '') {
     return (
       <div className={styles.wrap}>
-        <button className={styles.button} type="button" onClick={() => handleAnswerClick({ id: '2' })}>{letter.value}</button>
+        <button className={styles.button} type="button" onClick={() => handleAnswerClick({ id: '2' })}>{letter.choiceLetter}</button>
       </div>
     );
   }
   return (
     <div className={styles.wrap}>
-      <span>{letter.value}</span>
+      <span>{letter.choiceLetter}</span>
     </div>
   );
 };
