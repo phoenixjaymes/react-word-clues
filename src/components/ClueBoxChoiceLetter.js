4da8d52886d427354c57a39bf30f3c944cdaac25
Jaymes Young <jaymes@phoenixjaymes.com>
2019-12-24 13:30:37 -0800

    passed handleChoiceClick prop, added it to button

--- src/components/ClueBoxChoiceLetter.js
+++ src/components/ClueBoxChoiceLetter.js
@@ -3,17 +3,18 @@ import PropTypes from 'prop-types';
 
 import styles from '../css/clueBoxChoiceLetter.module.css';
 
-const ClueLetter = ({ children }) => {
+const ClueLetter = ({ children, handleChoiceClick }) => {
 
   return (
     <div className={styles.wrap}>
-      <button className={styles.button} type="button">{children}</button>
+      <button className={styles.button} type="button" onClick={() => handleChoiceClick({ id: '1' })}>{children}</button>
     </div>
   );
 };
 
 ClueLetter.propTypes = {
   children: PropTypes.string,
+  handleChoiceClick: PropTypes.func,
 };
 
 export default ClueLetter;
