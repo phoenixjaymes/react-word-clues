5898c403eef37846d5bd511add3a33aca21b36be
Jaymes Young <jaymes@phoenixjaymes.com>
2019-12-23 18:34:14 -0800

    changed css file name

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import Header from './Header';
 import Footer from './Footer';
 
-import '../css/layout.css';
+import '../css/layout.module.css';
 
 const Layout = ({ handleSectionClick, children }) => (
   <div>
