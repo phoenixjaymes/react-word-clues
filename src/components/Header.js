83de63d33f2db61489e59ef50594c3b8b2abf72e
Jaymes Young <jaymes@phoenixjaymes.com>
2019-12-23 17:06:27 -0800

    changed header.css to header.module.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import '../css/header.css';
+import '../css/header.module.css';
 
 const Header = ({ handleSectionClick }) => (
   <header>
