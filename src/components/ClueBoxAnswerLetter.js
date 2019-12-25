d78d00420ad4fd89865de9612f9d748d057ff7d6
Jaymes Young <jaymes@phoenixjaymes.com>
2019-12-24 17:56:58 -0800

    added answer object to pass to onClick handler

--- src/components/ClueBoxAnswerLetter.js
+++ src/components/ClueBoxAnswerLetter.js
@@ -4,10 +4,16 @@ import PropTypes from 'prop-types';
 import styles from '../css/clueBoxAnswerLetter.module.css';
 
 const ClueBoxAnswerLetter = ({ letter, handleAnswerClick }) => {
+  const answerObj = {
+    positionId: letter.id,
+    choiceId: letter.choiceId,
+    choiceLetter: letter.letter,
+  };
+
   if (letter.choiceLetter !== '') {
     return (
       <div className={styles.wrap}>
-        <button className={styles.button} type="button" onClick={() => handleAnswerClick({ id: '2' })}>{letter.choiceLetter}</button>
+        <button className={styles.button} type="button" onClick={() => handleAnswerClick(answerObj)}>{letter.choiceLetter}</button>
       </div>
     );
   }
