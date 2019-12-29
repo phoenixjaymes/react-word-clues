3c5adc0d4e6556cbae5a887877e5d31e1cf1d1aa
Jaymes Young <jaymes@phoenixjaymes.com>
2019-12-29 15:38:54 -0800

    Removed Shuflle
    Moved shuffle function in to ClueBox component.

--- src/components/ClueBoxChoice.js
+++ src/components/ClueBoxChoice.js
@@ -6,24 +6,7 @@ import ClueBoxChoiceLetter from './ClueBoxChoiceLetter';
 import styles from '../css/clueBoxChoice.module.css';
 
 const ClueBoxChoice = ({ choiceWordArray, handleChoiceClick, answerIsCorrect }) => {
-  function shuffle(array) {
-    const oldArray = [...array];
-    const newArray = [];
-
-    for (let i = array.length - 1; i > 0; i -= 1) {
-      const j = Math.floor(Math.random() * (i + 1));
-
-      const tempArray = oldArray.splice(j, 1);
-      newArray.push(tempArray[0]);
-    }
-
-    newArray.push(oldArray[0]);
-    return newArray;
-  }
-
-  const wordArray = shuffle(choiceWordArray);
-
-  const choiceLetters = wordArray.map((letter) => (
+  const choiceLetters = choiceWordArray.map((letter) => (
     <ClueBoxChoiceLetter key={letter.id} letter={letter} handleChoiceClick={handleChoiceClick} />
   ));
 
