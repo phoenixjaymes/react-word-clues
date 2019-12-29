ccd845eecb471b57ac4994cb398b0783f54befc8
Jaymes Young <jaymes@phoenixjaymes.com>
2019-12-29 13:36:40 -0800

    Aded answerIsCorrect
    Added answerIsCorrect and passed it to ClueBoxAnswerLetter

--- src/components/ClueBoxAnswer.js
+++ src/components/ClueBoxAnswer.js
@@ -5,13 +5,19 @@ import ClueBoxAnswerLetter from './ClueBoxAnswerLetter';
 
 import styles from '../css/clueBoxAnswer.module.css';
 
-const ClueBoxAnswer = ({ answerWordArray, isCheckingAnswer, handleAnswerClick }) => {
+const ClueBoxAnswer = ({
+  answerIsCorrect,
+  answerWordArray,
+  isCheckingAnswer,
+  handleAnswerClick,
+}) => {
   const answerLetters = answerWordArray.map((letter) => (
     <ClueBoxAnswerLetter
       key={letter.id}
       letter={letter}
       isCheckingAnswer={isCheckingAnswer}
       handleAnswerClick={handleAnswerClick}
+      answerIsCorrect={answerIsCorrect}
     />
   ));
 
@@ -23,6 +29,7 @@ const ClueBoxAnswer = ({ answerWordArray, isCheckingAnswer, handleAnswerClick })
 };
 
 ClueBoxAnswer.propTypes = {
+  answerIsCorrect: PropTypes.bool,
   answerWordArray: PropTypes.arrayOf(PropTypes.object),
   isCheckingAnswer: PropTypes.bool,
   handleAnswerClick: PropTypes.func,
