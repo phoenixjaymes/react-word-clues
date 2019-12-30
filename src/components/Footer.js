15ce7cdbe6f09a98289ead73c21408560965b3e9
Jaymes Young <jaymes@phoenixjaymes.com>
2019-12-29 16:27:03 -0800

    Imported style module
    Imported style module and added footer className
    Removed div with inner-container className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
+import styles from '../css/header.module.css';
+
 const Footer = () => {
   const currYear = new Date().getFullYear();
 
   return (
-    <footer>
-      <div className="inner-container">
-        <small>{`\u00A9 ${currYear} PhoenixJaymes`}</small>
-      </div>
+    <footer className={styles.footer}>
+      <small>{`\u00A9 ${currYear} PhoenixJaymes`}</small>
     </footer>
   );
 };
