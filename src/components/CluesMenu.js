736495272d3ac888f4fe2979b16cc2105d8f570e
Jaymes Young <jaymes@phoenixjaymes.com>
2019-12-30 11:35:11 -0800

    initial commit

---
+++ src/components/CluesMenu.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Layout from './Layout';
+import styles from '../css/clueMenu.module.css';
+
+const CluesMenu = ({ wordsData, handleSectionClick }) => {
+  const menu = wordsData.map((language) => (
+    <div key={language.id}>
+      <h2 className={styles.languageHeader}>{language.name}</h2>
+      {language.data.map((category) => (
+        <ul key={category.id}>
+          <li>
+            <h3 className={styles.categoryHeader}>
+              {category.category.charAt(0).toUpperCase() + category.category.slice(1)}
+            </h3>
+
+            <ul className={styles.subMenu}>
+              {category.data.map((group) => {
+                const menuObj = {
+                  language: language.language,
+                  category: category.category,
+                  group: group.id,
+                };
+
+                return (
+                  <li key={group.id}>
+                    <button
+                      className={styles.button}
+                      type="button"
+                      onClick={() => handleSectionClick('clues', menuObj)}
+                    >
+                      {group.name}
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+
+          </li>
+
+        </ul>
+      ))}
+    </div>
+  ));
+
+  return (
+    <Layout
+      header="Clues Menu"
+      handleSectionClick={handleSectionClick}
+    >
+      <nav className={styles.nav}>
+        {menu}
+      </nav>
+    </Layout>
+  );
+};
+
+CluesMenu.propTypes = {
+  wordsData: PropTypes.arrayOf(PropTypes.object),
+  handleSectionClick: PropTypes.func,
+};
+
+export default CluesMenu;
