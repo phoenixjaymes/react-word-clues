166843a44a7b8ec8465958c388e2c92214a683e3
Jaymes Young <jaymes@phoenixjaymes.com>
2019-12-30 11:18:02 -0800

    Added getWordList func and changed variable names
    Created getWordList func and added to handleeSectionClick func
    Changed wordsData to initialWordsData. and changed state to coincide.
    Added wordList state and changed CluesMain words prop to wordList
    Added another if block for CluesMenu componet.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/App.css';
 
 import Home from './Home';
 import CluesMain from './CluesMain';
+import CluesMenu from './CluesMenu';
 
-import wordsData from '../data/words';
+import initialWordsData from '../data/words';
 
 function App() {
-  const [words, setWords] = useState(wordsData);
-  const [section, setSection] = useState('clues');
+  const [wordsData, setWordsData] = useState(initialWordsData);
+  const [wordList, setWordList] = useState([]);
+  const [section, setSection] = useState('menu');
 
-  const handleSectionClick = (sectionName) => {
+  useEffect(() => {
+    setWordsData(initialWordsData);
+  }, []);
+
+
+  const getWordList = (menuObj) => {
+    const languageObject = wordsData.find((item) => item.language === menuObj.language);
+    const categoryObject = languageObject.data.find((item) => item.category === menuObj.category);
+    const groupObject = categoryObject.data.find((item) => item.id === menuObj.group);
+
+    setWordList(groupObject.data);
+  };
+
+  const handleSectionClick = (sectionName, menuObj = undefined) => {
+    if (menuObj !== undefined) {
+      getWordList(menuObj);
+    }
+
     setSection(sectionName);
   };
 
@@ -18,7 +37,16 @@ function App() {
     return (
       <CluesMain
         handleSectionClick={handleSectionClick}
-        words={words}
+        wordList={wordList}
+      />
+    );
+  }
+
+  if (section === 'menu') {
+    return (
+      <CluesMenu
+        handleSectionClick={handleSectionClick}
+        wordsData={wordsData}
       />
     );
   }
