0d359f6d987e2dbd78244a09928e8237f552a62e
Jaymes Young <jaymes@phoenixjaymes.com>
2020-01-01 14:26:02 -0800

    Filled in home compoent
    Added text and image to home component.
    Imported style module and added className to div.
    Imported and added an image of Köln

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Layout from './Layout';
+import koln from './cologne-640.jpg';
+import styles from '../css/home.module.css';
 
 const Home = ({ handleSectionClick }) => (
   <Layout
     header="Home"
     handleSectionClick={handleSectionClick}
   >
-    <p>Stuff</p>
+    <div className={styles.container}>
+      <img src={koln} alt="Köln at night" />
+      <p>If you aren’t planning a trip anytime soon. You may need to brush up on you German of Dutch vocabulary? Specifcally food, clothing and other basics.</p>
+      <p>Then this is the place to do it and have fun in the process. Practice here with this simple word scramble game.</p>
+      <p>Simply figure out the clue and unscramble the letters. That's all that you have to do. Simple, easy and a little bit of fun.</p>
+    </div>
   </Layout>
 );
 
